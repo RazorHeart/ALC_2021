1c3e1a7d753ee3bdecb6e4a39fb2233e301ca240
Mason <razorblade7676@gmail.com>
2021-09-30 09:07:22 -0600

    Finished 2D game, started migration of assets

---
+++ GameDev2020/Projects/Prototype2-Top Down Game B/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public bool hasKey;
+    public bool isdoorlocked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasKey = false;
+        isdoorlocked = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player unlocks the door and exits the room
+        if(hasKey && !isdoorlocked )
+            {
+                print("Door Unlocked");
+            }
+    }
+}
