0755f55f659e5b2f3a113097b569dfe3ec76b4a0
Mason <razorblade7676@gmail.com>
2021-12-07 09:17:01 -0700

    semi-Final PlayerController for personal project

--- GameDev2020/Projects/Primitive-personalproject/Assets/Scripts/PlayerControls.cs
+++ GameDev2020/Projects/Primitive-personalproject/Assets/Scripts/PlayerControls.cs
@@ -4,35 +4,68 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-
+    //player movement
     public float moveSpeed;
     public float jumpForce;
-    public float hInput;
-    private Rigidbody rigidbody;
+    //ground check
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+    //Non-Stick Player?
+    private float moveVelocity;
+    //for animation use later or drop
+    public Animator animator;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //also for animation
+        animator.SetBool("isWalking", false);
+        animator.SetBool("isJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Jump"))
-            Jump();
-        hInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime * hInput);
+        if(Input.GetKeyDown(KeyCode.Space)&& grounded)
+            {
+                Jump();
+            }
+
+        moveVelocity = 0f;
+
+        if(Input.GetKey (KeyCode.D))
+            {
+                moveVelocity = moveSpeed;
+                animator.SetBool("isWalking", true);
+            }
+        else if (Input.GetKeyUp(KeyCode.D))
+            {
+                animator.SetBool("isWalking", false);
+            }
+        if(Input.GetKey (KeyCode.A))
+            {
+                moveVelocity = -moveSpeed;
+                animator.SetBool("isWalking", true);
+            }
+        else if (Input.GetKeyUp(KeyCode.A))
+            {
+                animator.SetBool("isWalking", false);
+            }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<RigidBody2D>().velocity.y);
     }
 
-    void Jump()
+    public void Jump()
     {   
-        // cast ray to ground
-        //Ray ray = new Ray(transform.position, Vector3.down);
-        //checks ray to determine if on the ground
-        //if(Physics.Raycast(ray, 1.1f))
-        //    rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-
+        GetComponent<RigidBody2D>().velocity = new Vector2(GetComponent<RigidBody2D>().velocity.x, jumpForce);
+        animator.SetBool("isJumping",true);
     }
 
 }
